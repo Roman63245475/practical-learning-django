ba9a405c13f2cbcce5f0e9cf5a995780d5e7fb32
Roman <yummison44@gmail.com>
2025-05-30 14:38:35 +0200

    Creating a pagination, and sending an e-mail opportunity

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', post_list, name='post_list'),
-    path('post/<int:post_id>/', post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post_slug>', post_detail, name='post_detail'),
+    path('share/<slug:post_slug>', post_share, name='post_share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post/list.html', {'posts': posts, 'page_obj': page_obj})
 
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post_slug):
+    post = get_object_or_404(Post, slug=post_slug,
+                             status=Post.Status.PUBLISHED,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
 
 
-
+def post_share(request, post_slug):
+    post = get_object_or_404(Post, slug=post_slug)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you to read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'form': form, 'post': post, 'sent': sent})
