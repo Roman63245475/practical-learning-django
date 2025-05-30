508b17ecf39711f746d7c16a4dc07c2125c7d9f9
Roman <yummison44@gmail.com>
2025-05-30 15:40:03 +0200

    Implementing a comment system

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post_slug>', post_detail, name='post_detail'),
     path('share/<slug:post_slug>', post_share, name='post_share'),
+    path('comment/<slug:post_slug>', post_comment, name='post_comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
+from django.views.decorators.http import require_POST
+
 
 def post_list(request):
     posts = Post.published.all()
@@ -19,7 +21,10 @@ def post_detail(request, year, month, day, post_slug):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_slug):
@@ -37,3 +42,16 @@ def post_share(request, post_slug):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'form': form, 'post': post, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_slug):
+    post = get_object_or_404(Post, slug=post_slug)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
