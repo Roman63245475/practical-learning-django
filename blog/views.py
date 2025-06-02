24d1464a14cfcca015951725ecf51e5c61e85065
Roman <yummison44@gmail.com>
2025-06-02 18:31:04 +0200

    Adding a recommendation system to the single post

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -30,7 +31,14 @@ def post_detail(request, year, month, day, post_slug):
 
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.all().filter(tags__in=post_tags_ids).exclude(pk=post.pk)
+    sorted_similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    context = {'post': post,
+               'comments': comments,
+               'form': form,
+               'similar_posts': sorted_similar_posts, }
+    return render(request, 'blog/post/detail.html', context=context)
 
 
 def post_share(request, post_slug):
