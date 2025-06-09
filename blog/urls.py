0e1395f1ab89e8e35c5191dc78d6f276c355104b
Roman <yummison44@gmail.com>
2025-06-09 14:39:53 +0200

    Implementing a search system and changing database to PostgreSQL

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('share/<slug:post_slug>', post_share, name='post_share'),
     path('comment/<slug:post_slug>', post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>', post_list, name='posts_by_tag'),
-    path('feeds/', LatestPostsFeed(), name='post_feed')
+    path('feeds/', LatestPostsFeed(), name='post_feed'),
+    path('search/', post_search, name='post_search'),
 ]
